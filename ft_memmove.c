561dc2cc66eac7a6ce3e7861123a8e08ac36f4e3
mcarton <mcarton@student.s19.be>
2024-10-08 20:43:13 +0200

    unsigned char

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/08 20:35:23 by mcarton           #+#    #+#             */
-/*   Updated: 2024/10/08 20:35:23 by mcarton          ###   ########.fr       */
+/*   Created: 2024/10/08 08:54:36 by mcarton           #+#    #+#             */
+/*   Updated: 2024/10/08 08:54:36 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
-void ft_memset(void *tableau, int value, size_t size)
+
+void ft_memmove(char *dest, char *src, size_t size)
 {
-        size_t i;
-        unsigned char *tmp;
+    int i;
 
-        i = 0;
-        tmp = (unsigned char *) tableau;
-        while (i < size)
-        {
-                tmp[i] = (unsigned char) value;
-                i++;
-        }
+    i = 0;
+
+}
+
+int main(void)
+{
+    char dest[50];
+    char src[50] = "TestCarJeComprendsPas";
+    size_t size = sizeof(src);
+    memmove(src, src + 12, 4);
+    printf("Resultat avec chevauchement : %s\n", src);
 }
